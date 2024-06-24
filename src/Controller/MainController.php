bcff6486e51f864d4a2b3dd4482b4e1695c47043
Anna Konstantynova <konstanna.dev@gmail.com>
2024-06-24 10:54:37 +0200

    Add homepage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,28 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Hello');
+        $contracts = 150;
+
+        $myData = [
+            'nummer' => 12345,
+            'vorgangsart' => 'Transportrechnung',
+            'betreff' => 'Test Rechnung',
+            'kunde' => 'DB',
+            'betrag' => 235,
+        ];
+        
+        return $this->render('main/homepage.html.twig', [
+            'contracts' => $contracts,
+            'myData' => $myData,
+        ]);
     }
 }
