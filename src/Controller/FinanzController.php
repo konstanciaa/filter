999d3764477f9681283d4a1645cf738a81bdca3b
Anna Konstantynova <konstanna.dev@gmail.com>
2024-06-25 10:30:50 +0200

    Create filter form

--- src/Controller/FinanzController.php
+++ src/Controller/FinanzController.php
@@ -2,18 +2,37 @@
 
 namespace App\Controller;
 
+use App\Form\FinanzFilterType;
 use App\Repository\FinanzbuchhaltungRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class FinanzController extends AbstractController
 {
     #[Route('/finanz', name: 'app_finanz')]
-    public function index(FinanzbuchhaltungRepository $finanzbuchhaltungRepository): Response
+    public function index(Request $request, FinanzbuchhaltungRepository $finanzbuchhaltungRepository): Response
     {
-        $finanz = $finanzbuchhaltungRepository->findAll();
-        dd($finanz);
-        return $this->render('finanz/index.html.twig');
+        $form = $this->createForm(FinanzFilterType::class);
+        $form->handleRequest($request);
+
+        $finanz = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $type = $form->get('vorgangsart')->getData();
+            if ($type) {
+                $finanz = $finanzbuchhaltungRepository->findBy(['vorgangsart' => $type]);
+            } else {
+                $finanz = $finanzbuchhaltungRepository->findAll();
+            }
+        } else {
+            $finanz = $finanzbuchhaltungRepository->findAll();
+        }
+
+        return $this->render('finanz/index.html.twig', [
+            'form' => $form->createView(),
+            'finanz' => $finanz,
+        ]);
     }
 }
